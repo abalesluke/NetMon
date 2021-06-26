6c08909c80a5f95295704ce5801788d5e91b8ed5
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-06-27 03:37:28 +0800

    Update netmon.py

--- netmon.py
+++ netmon.py
@@ -1,35 +1,23 @@
-import os, socket, sys
-try:
-	from getmac import get_mac_address
-	from getmac import getmac
-	from mac_vendor_lookup import MacLookup
+import os
+from getmac import get_mac_address
+from getmac import getmac
+from mac_vendor_lookup import MacLookup
 
-except:
-	os.system('pip3 install getmac')
-	os.system('pip3 install mac_vendor_lookup')
+while True:
+ host = '192.168.0.'
+ print("="*55)
+ print("        MAC          |        Vendor                   ")
+ print("="*55)
+ for i in range(1, 254):
+  client = host+str(i)
+  mac = getmac.get_mac_address(ip=client, network_request=True)
+  try:
+   if mac == "00:00:00:00:00:00":
+    pass
+   else:
+   	os.system(f'echo {mac} >> mac.txt')
+   	vendor = MacLookup().lookup(mac)
+   	print(f' {mac}  :|: {vendor}  ')
 
-print('started')
-
-for i in range(1, 254):
-	ip = f'192.168.0.{i}'
-	for port in range(100):
-		done = False
-		if done == True:
-			done = False
-			break
-		else:
-			pass
-		try:
-			s = socket.socket()
-			s.settimeout(.1)
-			s.connect((f'{ip}', port))
-			sys.stdout.write('\r                               ')
-			print(f'\r[Open {port}] {ip} ')
-			mac = getmac.get_mac_address(ip=ip, network_request=True)
-			print('\r[MAC ] '+mac+'')
-			print(f'\r[Vendor] {MacLookup().lookup(mac)}\n')
-			done = True
-			break
-		except:
-			sys.stdout.write(f'\r[Scanning] {port} {ip}   ')
-			
+  except:
+   pass
